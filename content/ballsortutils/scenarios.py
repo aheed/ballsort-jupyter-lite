83f7a2dfa66f2afb5d00375e90b6bcce78dfb5c2
Anders Heed <aheed88888@gmail.com>
2023-08-31 16:05:34 +0200

    updated content

--- content/ballsortutils/scenarios.py
+++ content/ballsortutils/scenarios.py
@@ -9,11 +9,14 @@ async def set_scenario1(sc: ScenarioControl):
         StateBall(pos=StatePosition(x=1, y=4), color="green"),
     ]
     await sc.set_scenario(balls)
+    print("Goal: All marbles shall be in the leftmost column")
 
-async def set_scenario2(sc: ScenarioControl):
+async def set_challenge1_scenario(sc: ScenarioControl):
     balls = [
-        StateBall(pos=StatePosition(x=0, y=2), color="yellow"),
-        StateBall(pos=StatePosition(x=0, y=3), color="yellow"),
         StateBall(pos=StatePosition(x=0, y=4), color="pink"),
+        StateBall(pos=StatePosition(x=0, y=3), color="yellow"),
+        StateBall(pos=StatePosition(x=0, y=2), color="pink"),
+        StateBall(pos=StatePosition(x=1, y=4), color="yellow"),
     ]
-    await sc.set_scenario(balls)    
+    await sc.set_scenario(balls)
+    print("Goal: Yellow and pink marbles in separate columns")
