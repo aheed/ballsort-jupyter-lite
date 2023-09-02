be9ceaf11f26a3d588cdf2aecb206ae8d8428cea
Anders Heed <aheed88888@gmail.com>
2023-09-02 10:17:08 +0200

    add state_utils

---
+++ content/ballsortutils/state_utils.py
@@ -0,0 +1,31 @@
+from state_update_model import (
+    StateModel,
+    MAX_Y,
+)
+
+
+def is_ball_in_claw(state: StateModel) -> bool:
+    return not not state.claw.ball_color
+
+
+def is_ball_at_current_pos(state: StateModel) -> bool:
+    return next(
+        (True for ball in state.balls if ball.pos == state.claw.pos),
+        False,
+    )
+    # balls_at_current_pos = filter(lambda ball: ball.pos == self.state.claw.pos, self.state.balls)
+    # return any(balls_at_current_pos)
+
+
+def get_top_occupied_index(state: StateModel) -> int:
+    y_indexes_in_current_column = [
+        ball.pos.y for ball in state.balls if ball.pos.x == state.claw.pos.x
+    ]
+    top_occupied_y_index = (
+        max(y_indexes_in_current_column) if y_indexes_in_current_column else MAX_Y
+    )
+    return top_occupied_y_index
+
+
+def get_top_vacant_index(state: StateModel) -> int:
+    return get_top_occupied_index(state) - 1
--- content/ballsortutils/state_validator.py
+++ content/ballsortutils/state_validator.py
@@ -1,5 +1,11 @@
 from dataclasses import dataclass
 from ball_control import IllegalBallControlStateError
+from state_utils import (
+    get_top_occupied_index,
+    get_top_vacant_index,
+    is_ball_at_current_pos,
+    is_ball_in_claw,
+)
 from state_update_model import (
     StateModel,
     MIN_X,
@@ -15,57 +21,32 @@ class StateValidator:
 
     state: StateModel
 
-    def _is_ball_in_claw(self) -> bool:
-        return not not self.state.claw.ball_color
-    
-    def _is_ball_at_current_pos(self) -> bool:
-        return next(
-            (True for ball in self.state.balls if ball.pos == self.state.claw.pos),
-            False,
-        )
-        # balls_at_current_pos = filter(lambda ball: ball.pos == self.state.claw.pos, self.state.balls)
-        # return any(balls_at_current_pos)
-
-    def _get_top_occupied_index(self) -> int:
-        y_indexes_in_current_column = [
-            ball.pos.y
-            for ball in self.state.balls
-            if ball.pos.x == self.state.claw.pos.x
-        ]
-        top_occupied_y_index = (
-            max(y_indexes_in_current_column) if y_indexes_in_current_column else MAX_Y
-        )
-        return top_occupied_y_index
-
-    def _get_top_vacant_index(self) -> int:
-        return self._get_top_occupied_index() - 1
-
     def move_relative(self, x: int, y: int):
         newX = self.state.claw.pos.x + x
         newY = self.state.claw.pos.y + y
         if newX < MIN_X or newY < MIN_Y or newX > MAX_X or newY > MAX_Y:
             raise IllegalBallControlStateError("coordinates out of bounds")
 
-    def move_horizontally(self, distance: int):
-        self.move_relative(x=distance, y=0)
-
-    def move_vertically(self, distance: int) -> None:
-        self.move_relative(x=0, y=distance)
+    #    def move_horizontally(self, distance: int):
+    #        self.move_relative(x=distance, y=0)
+    #
+    #    def move_vertically(self, distance: int) -> None:
+    #        self.move_relative(x=0, y=distance)
 
     def open_claw(self):
-        if not self._is_ball_in_claw():
+        if not is_ball_in_claw(self.state):
             return
 
-        if self.state.claw.pos.y != self._get_top_occupied_index():
+        if self.state.claw.pos.y != get_top_occupied_index(self.state):
             raise IllegalBallControlStateError(
                 "Illegal drop location. Must be topmost vacant position."
             )
 
     def close_claw(self):
-        if not self._is_ball_at_current_pos():
+        if not is_ball_at_current_pos(self.state):
             return
 
-        if self.state.claw.pos.y != self._get_top_vacant_index():
+        if self.state.claw.pos.y != get_top_vacant_index(self.state):
             raise IllegalBallControlStateError(
                 "Illegal grab. Must be topmost marble position."
             )
