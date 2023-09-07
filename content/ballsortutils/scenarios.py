c7604cc525628cda77eda387ffcfd06ad7efa16d
Anders Heed <aheed88888@gmail.com>
2023-09-07 22:25:31 +0200

    cleanup

--- content/ballsortutils/scenarios.py
+++
@@ -1,9 +0,0 @@
-from ch0_scenario import Ch0Scenario
-from ch1_scenario import Ch1Scenario
-from scenario_control import ScenarioControl
-
-async def set_scenario1(sc: ScenarioControl):
-    await sc.set_scenario(Ch0Scenario())
-
-async def set_challenge1_scenario(sc: ScenarioControl):
-    await sc.set_scenario(Ch1Scenario())
